e4586a421cdb46137e2baea1e8a2eedef758863e
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-05-03 10:34:37 -0500

    added pool.js

---
+++ server/modules/pool.js
@@ -0,0 +1,24 @@
+const pg = require('pg');
+const Pool = pg.Pool;
+
+// Pool is created with these settings,
+// Noting that it is connected to the 'koalaholla' database
+const pool = new Pool({
+    database: 'koalaholla',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+});
+
+// If it actually connects, leave us a message!
+pool.on('connect', () => {
+    console.log('Pool connected!');
+});
+
+// If it does not connect, log an error
+pool.on('error', (err) => {
+    console.log('DB Error: ', err);
+});
+
+module.exports = pool;
