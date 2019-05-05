df865ddcde9edbe86f54367d68c6cb55d690ae3c
Konou Vang <konouvang@Konous-MacBook-Pro.local>
2019-05-05 16:02:09 -0500

    client.js updated

--- server/routes/task.router.js
+++ server/routes/task.router.js
@@ -1,16 +1,16 @@
 const express = require('express');
-const koalaRouter = express.Router();
+const taskRouter = express.Router();
 
 
 // DB CONNECTION
 const pool = require('../modules/pool');
 
 // GET
-koalaRouter.get('/', (req,res) => {
+taskRouter.get('/', (req,res) => {
     // Builds string to use in database later
     // This string will be used to get all data from the
     // restaurants table
-    const queryString = `SELECT * FROM koalaholla ORDER BY "name" ASC;`;
+    const queryString = `SELECT * FROM weekend-to-do-app ORDER BY "name" ASC;`;
 
     // Use the connection from the database (called 'pool')
     // and use the query string above, to get the data
@@ -30,24 +30,24 @@ koalaRouter.get('/', (req,res) => {
 });
 
 // POST
-koalaRouter.post('/', (req,res) => {
+taskRouter.post('/', (req,res) => {
     // Create a variable that has a name reflective of the name it
     // had on the client side. Set this equal to the req.body,
     // which was provided to us by body-parser
-    const koalaObject = req.body;
+    const taskObject = req.body;
 
-    console.log(koalaObject);
+    console.log(taskObject);
 
     // Setting up the query string used for SQL later to save the 
     // data in. Note the $1,$2,$3 will eventually map to values
     // we enter when we get to the actual query
-    const queryString = `INSERT INTO "koalaholla" ("name", "gender", "age", "readyForTransfer", "notes")
+    const queryString = `INSERT INTO "weekend-to-do-app" ("name", "gender", "age", "readyForTransfer", "notes")
                     VALUES ($1,$2,$3,'false',$4);`;
 
     // Hey connection to the database, run a query with the
     // string we built above. Mapping the values inside the array
     // in the order they are provided.
-    pool.query(queryString, [koalaObject.name, koalaObject.gender, koalaObject.age, koalaObject.notes])
+    pool.query(queryString, [taskObject.name, taskObject.gender, taskObject.age, taskObject.notes])
         .then((response) => {
             // 201 tells the client that the server was able to
             // save to the database. 'Created' is what the client sees
@@ -63,8 +63,8 @@ koalaRouter.post('/', (req,res) => {
 
 
 // DELETE
-koalaRouter.delete('/delete/:id', (req, res) => {
-    const queryString = `DELETE FROM "koalaholla" WHERE id=$1;`;
+taskRouter.delete('/delete/:id', (req, res) => {
+    const queryString = `DELETE FROM "weekend-to-do-app" WHERE id=$1;`;
 
     pool.query(queryString, [req.params.id])
         .then((response) => {
@@ -74,10 +74,10 @@ koalaRouter.delete('/delete/:id', (req, res) => {
             console.log('Error delete: '.err);
         });
 }); // this is for the router
-module.exports = koalaRouter;
+module.exports = taskRouter;
 
-koalaRouter.put('/readyForTransfer/:id', (req,res) => {
-    const queryString = `UPDATE "koalaholla" SET "readyForTransfer"=true WHERE id=$1;`;
+taskRouter.put('/readyForTransfer/:id', (req,res) => {
+    const queryString = `UPDATE "weekend-to-do-app" SET "readyForTransfer"=true WHERE id=$1;`;
 
     pool.query(queryString, [req.params.id])
         .then((response) => {
